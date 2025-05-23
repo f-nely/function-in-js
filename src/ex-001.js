98552ebae99b341e84b0e21ecc1fee39b03ceeda
nelynely <nelly.nf54@gmail.com>
2025-05-23 16:22:26 -0300

    Create ex-001.js

---
+++ src/ex-001.js
@@ -0,0 +1,8 @@
+function multiplicador(number1) {
+  for (let i = 1; i <= 10; i++) {
+    //console.log(`${i} x ${number1} = ${i * number1}`);
+    console.log(i + " x" + number1 + i * number1);
+  }
+}
+
+multiplicador(4);
